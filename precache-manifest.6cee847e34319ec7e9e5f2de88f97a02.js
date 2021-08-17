1b9c142ef55e1c8a503ab6ce59c57941f463f839
travis chan <travischan@ps.asl.com.hk>
2021-08-17 12:02:02 +0800

    Updates

---
+++ precache-manifest.6cee847e34319ec7e9e5f2de88f97a02.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e36b28dbc6947a47fabbf58a731f3cbd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c7b386ae1ba07dcb5dbb",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "83dce78d4f9865feace6",
+    "url": "/static/js/2.b3f8d31d.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/static/js/2.b3f8d31d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c7b386ae1ba07dcb5dbb",
+    "url": "/static/js/main.e20f23bb.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
