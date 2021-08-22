56fead8689d7a44dc985ecf99d8a093336976080
travis chan <travischan@ps.asl.com.hk>
2021-08-22 13:53:52 +0800

    add pull to refresh

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,15 +1,24 @@
 import React, {useEffect} from 'react';
-import {Center, Container, Text, VStack} from '@chakra-ui/react';
+import {Box, Center, CircularProgress, Container, Divider, Text, VStack} from '@chakra-ui/react';
 import {useActions, useAppState} from '../overminder';
 import {PushMessage, PushMessageProps} from '../components/PushMessage';
+import PullToRefresh from 'react-simple-pull-to-refresh';
+import {DownloadIcon} from '@chakra-ui/icons';
 
 interface MessageContainerProps {
   messages: Array<PushMessageProps>;
 }
 
-const MessageContainer = ({messages}: MessageContainerProps) =>  {
+const MessageContainer = ({messages}: MessageContainerProps) => {
   return (
     <VStack maxW={'100%'} p={'0.5em'}>
+      <Box w={'100%'}>
+        {/*<Divider />*/}
+        <Center>
+          <DownloadIcon/>
+          Pull down to refresh
+        </Center>
+      </Box>
       {messages.map((m, i) => (
         <PushMessage key={'push' + i} {...m} />
       ))}
@@ -17,15 +26,20 @@ const MessageContainer = ({messages}: MessageContainerProps) =>  {
   );
 };
 
+export const RefreshingContent = ()=>(
+  <CircularProgress isIndeterminate color="green.300" />
+)
 export const Main = () => {
   const {fetchAndSetAllPushes} = useActions();
-  const {pushes,accessToken} = useAppState();
+  const {pushes, accessToken} = useAppState();
   useEffect(() => {
     fetchAndSetAllPushes();
   }, [accessToken]);
   return (
-    <Container maxW={'100vw'}>
-      <MessageContainer messages={pushes} />
-    </Container>
+    <PullToRefresh onRefresh={fetchAndSetAllPushes} pullingContent={''} refreshingContent={<RefreshingContent />} >
+      <Container maxW={'100vw'}>
+        <MessageContainer messages={pushes} />
+      </Container>
+    </PullToRefresh>
   );
 };
